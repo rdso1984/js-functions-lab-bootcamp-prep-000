3832f212baa92b359a2925ebb5927fdb1d613531
Rafael Oliveira <rdso1984@gmail.com>
2017-10-05 15:08:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo (${name}) {
-  return `Happy holidays, ${name}!`
+function happyHolidaysTo (string) {
+  return `Happy holidays,`+ string
 }
 
 function happyHolidayTo(${holiday}, ${name}) {
